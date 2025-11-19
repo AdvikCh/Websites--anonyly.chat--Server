9c87a757ec88acb7d546de41b1e10597d7bafcbd
advikch <114327829+AdvikCh@users.noreply.github.com>
2025-11-19 22:03:04 +0530

    Living it on render.com

---
+++ Source code-Server.py
@@ -0,0 +1,16 @@
+import trio; from trio_websocket import serve_websocket, ConnectionClosed; import os
+
+clients = set(); PORT = int(os.environ.get("PORT", 10000))
+
+async def relay(request):
+    web_socket = await request.accept(); clients.add(web_socket)
+    try:
+        while True:
+            data = await web_socket.get_message(); print(data.decode())
+            for client in clients: await client.send_message(data)
+    except ConnectionClosed: pass
+    finally: clients.remove(web_socket)
+
+async def main(): await serve_websocket(relay, '0.0.0.0', PORT)
+
+trio.run(main)
