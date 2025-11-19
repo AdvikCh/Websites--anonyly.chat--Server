091b17c9a5c051e3e70c3d1cb32fafdc54962ef5
advikch <114327829+AdvikCh@users.noreply.github.com>
2025-11-19 22:51:41 +0530

    Living it on render.com

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ async def relay(request):
     web_socket = await request.accept(); clients.add(web_socket)
     try:
         while True:
-            data = await web_socket.get_message(); print(data.decode())
+            data = await web_socket.get_message(); print(data)
             for client in clients: await client.send_message(data)
     except ConnectionClosed: pass
     finally: clients.remove(web_socket)
@@ -14,4 +14,5 @@ async def relay(request):
 async def main(): await serve_websocket(relay, '0.0.0.0', PORT, ssl_context=None)
 
 trio.run(main)
+
 
